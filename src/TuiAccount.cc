0de31558315469c57435bbcedb9b223ed1cd2b05
Simone Pagan Griso <simone.pagangriso@gmail.com>
2013-03-25 00:07:49 +0100

    reverted back auto-source selection due to refresh bug

--- src/TuiAccount.cc
+++ src/TuiAccount.cc
@@ -511,13 +511,13 @@ SourceURI TuiAccount::SelectSource()
   vector<SourceURI> allSources = ioSvcM->GetManagedSources();
   // disable the following when new source creation possible
   //----
-  if (allSources.size() == 0) {
-    m_errorMsg = "No source available for storing account.";
-    m_statusCode = SC_ERROR;
-    return SourceURI("");
-  }
-  if (allSources.size() == 1)
-    return allSources[0]; //no choice..
+  //if (allSources.size() == 0) {
+  //  m_errorMsg = "No source available for storing account.";
+  //  m_statusCode = SC_ERROR;
+  //  return SourceURI("");
+  //}
+  //if (allSources.size() == 1)
+  //  return allSources[0]; //no choice..
   //----
   SourceURI itemNewSource("Create new", "=>", "<="); //add special item for creating a new source
   allSources.push_back(itemNewSource);
